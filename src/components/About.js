c06e673ecda681563a0058a3a8a4321ddb3f6320
Samuel Rivero <samuelfras@gmail.com>
2023-08-12 14:39:22 -0600

    Lab Complete

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,16 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
-function About() {
+function About(props) {
+  const  {bio} = props
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      {bio ? <p>{bio}</p> : null }
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {<Links linkedin={user.links.linkedin}
+      github={user.links.github}/>}
     </div>
   );
 }
